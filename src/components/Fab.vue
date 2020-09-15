fddb5c66cc1664bacb989396b0767d58f21ae79f
İlyas <ashnwor@gmail.com>
2020-09-15 13:07:25 +0300

    general: fab speeddial

--- src/components/Fab.vue
+++ src/components/Fab.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-    <md-button class="md-fab">
-      <md-icon>menu</md-icon>
-    </md-button>
+    <md-speed-dial>
+      <md-speed-dial-target class="md-primary">
+        <md-icon>menu</md-icon>
+      </md-speed-dial-target>
+      <md-speed-dial-content>
+        <md-button class="md-icon-button">
+          <md-icon>invert_colors</md-icon>
+        </md-button>
+      </md-speed-dial-content>
+    </md-speed-dial>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 import TextareaAutosize from 'vue-textarea-autosize';
-import { MdButton, MdIcon } from 'vue-material/dist/components';
+import { MdButton, MdIcon, MdSpeedDial } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
 
 Vue.use(TextareaAutosize);
 Vue.use(MdButton);
 Vue.use(MdIcon);
+Vue.use(MdSpeedDial);
 
 Vue.config.productionTip = false;
 
